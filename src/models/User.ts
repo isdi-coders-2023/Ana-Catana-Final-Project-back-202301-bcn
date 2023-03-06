55cbc7aa4ef3224cdc7e47be1d59e34199d4f778
AnaCatanaR <anacatana.ac@gmail.com>
2023-03-06 22:44:41 +0100

    Add userSchema to project

---
+++ src/models/User.ts
@@ -0,0 +1,22 @@
+import { model, Schema } from "mongoose";
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 8,
+  },
+});
+
+const User = model("User", userSchema, "users");
+
+export default userSchema;
