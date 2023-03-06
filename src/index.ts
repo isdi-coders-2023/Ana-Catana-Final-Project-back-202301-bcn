4e63a17541c3b7b766028cda18cf6c144af1c69c
AnaCatanaR <anacatana.ac@gmail.com>
2023-03-06 21:22:32 +0100

    Add main index file and sonar coverage exclusions

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
+import "./loadEnviroments.js";
+import createDebug from "debug";
+import startServer from "./server/startServer.js";
+import connectDatabase from "./database/connectDatabase.js";
+
+export const debug = createDebug("users:*");
+
 const port = process.env.PORT ?? 4000;
+const mongoDdUrl = process.env.MONGODB_CONNECTION_URL!;
+
+try {
+  await connectDatabase(mongoDdUrl);
+  debug("Connected to data base");
+
+  await startServer(Number(port));
+  debug(`Server listening on port ${port}`);
+} catch (error) {
+  debug(error.message);
+}
