932f3fe9788bd674cc486d1804d1de7a43625415
AnaCatanaR <anacatana.ac@gmail.com>
2023-03-08 11:48:16 +0100

    Update  usersControler test for CustomError

--- src/server/controllers/userControllers.ts
+++ src/server/controllers/userControllers.ts
@@ -1,11 +1,16 @@
 import { type NextFunction, type Request, type Response } from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { type CustomJwtPayload, type UserStructure } from "./types.js";
+import { type UserLoginCredentials, type CustomJwtPayload } from "./types.js";
 import User from "../../models/User.js";
+import CustomError from "../../CustomError/CustomError.js";
 
 export const loginUser = async (
-  req: Request<Record<string, unknown>, Record<string, unknown>, UserStructure>,
+  req: Request<
+    Record<string, unknown>,
+    Record<string, unknown>,
+    UserLoginCredentials
+  >,
   res: Response,
   next: NextFunction
 ) => {
@@ -15,6 +20,8 @@ export const loginUser = async (
     const user = await User.findOne({ email }).exec();
 
     if (!user || !(await bcrypt.compare(password, user.password))) {
+      const error = new CustomError("User not found", 401, "User not found");
+      next(error);
       return;
     }
 
