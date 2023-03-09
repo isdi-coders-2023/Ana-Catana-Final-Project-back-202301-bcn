25e785662439e7b2ba65f67e5f523c7553e3822f
AnaCatanaR <anacatana.ac@gmail.com>
2023-03-09 12:08:52 +0100

    Add errorMiddlewares test

---
+++ src/server/controllers/middlewares/erroMiddlewares.test.ts
@@ -0,0 +1,66 @@
+import { type Request, type Response, type NextFunction } from "express";
+import CustomError from "../../../CustomError/CustomError";
+import { generalError, notFoundError } from "./errorMiddlewares";
+
+const response: Partial<Response> = {
+  status: jest.fn().mockReturnThis(),
+  json: jest.fn(),
+};
+const request: Partial<Request> = {};
+const next: Partial<NextFunction> = jest.fn();
+
+beforeEach(() => jest.clearAllMocks());
+
+describe("Given a notFoundError middleware", () => {
+  describe("When it receives a request", () => {
+    test("Then it should call its method", () => {
+      notFoundError(
+        request as Request,
+        response as Response,
+        next as NextFunction
+      );
+
+      expect(next).toHaveBeenCalled();
+    });
+  });
+});
+
+describe("Given a generalError middleware", () => {
+  describe("When it receives an error with status code '500'", () => {
+    test("Then it should call its status method with a ststus code '500'", () => {
+      const statusCode = 500;
+      const error = new CustomError(
+        "500 Internal Server Error",
+        500,
+        "Something went wrong"
+      );
+
+      generalError(
+        error,
+        request as Request,
+        response as Response,
+        next as NextFunction
+      );
+
+      expect(response.status).toHaveBeenCalledWith(statusCode);
+    });
+  });
+
+  describe("When it receives an error without status code or message", () => {
+    test("then it should call its status method with a code '500' and its json method with 'Something went wrong'", () => {
+      const error = new CustomError("", 0, "");
+      const expectedStatusCode = 500;
+      const expectedErrorMessage = { error: "Something went wrong" };
+
+      generalError(
+        error,
+        request as Request,
+        response as Response,
+        next as NextFunction
+      );
+
+      expect(response.status).toHaveBeenCalledWith(expectedStatusCode);
+      expect(response.json).toHaveBeenCalledWith(expectedErrorMessage);
+    });
+  });
+});
--- src/server/index.ts
+++ src/server/index.ts
@@ -3,6 +3,10 @@ import express from "express";
 import cors from "cors";
 import options from "../middlewares/cors.js";
 import userRouter from "./routers/userRouter.js";
+import {
+  generalError,
+  notFoundError,
+} from "./controllers/middlewares/errorMiddlewares.js";
 
 const app = express();
 
@@ -13,5 +17,8 @@ app.use(express.json());
 app.use(cors(options));
 
 app.use("/users", userRouter);
+
+app.use(notFoundError);
+app.use(generalError);
 
 export default app;
