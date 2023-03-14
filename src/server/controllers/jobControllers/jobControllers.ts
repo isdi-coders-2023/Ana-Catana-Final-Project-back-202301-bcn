e0257518ba8bf71dc30a9a567434635a55b7e5fc
AnaCatanaR <anacatana.ac@gmail.com>
2023-03-14 17:16:11 +0100

    Add jobControllers to project

---
+++ src/server/controllers/jobControllers/jobControllers.ts
@@ -0,0 +1,21 @@
+import { type NextFunction, type Request, type Response } from "express";
+import CustomError from "../../../CustomError/CustomError";
+import Job from "../../../database/models/Job";
+
+export const getJobs = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const jobs = await Job.find().exec();
+    res.status(200).json({ jobs });
+  } catch (error) {
+    const customError = new CustomError(
+      (error as Error).message,
+      500,
+      "No jobs found."
+    );
+    next(customError);
+  }
+};
